7816a7d99915477ed17cec5b37a401392c13fea4
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-11-05 17:28:01 +0100

    Copy libraries to dist alongside the rest of the code

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var paths = {
   srcComponentsJsx: "src/Components/*.jsx",
   srcJs: "src/*.js",
   srcHtml: "src/*.{html,css,png,json}",
+  lib: ["node_modules/react/dist/react.min.js"],
   dist: "dist/"
 };
 
@@ -23,5 +24,10 @@ gulp.task("default", function () {
     
   gulp
     .src(paths.srcHtml)
+    .pipe(gulp.dest(paths.dist));
+    
+  gulp
+    .src(paths.lib)
+    .pipe(gulpConcat("/lib.js"))
     .pipe(gulp.dest(paths.dist));
 });
