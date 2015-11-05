135f221e7084f496f5e071f643e2b60c0dcc0292
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-11-05 16:21:56 +0100

    Track issue add/remove and notify based on that
    Closes #8. Closes #11. Closes #13.

--- src/background.js
+++ src/background.js
@@ -1,6 +1,53 @@
+var model = null;
+
+chrome.notifications.onClosed.addListener(function (notificationId, byUser) {
+
+});
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+  for (var repository of model.repositories) {
+    for (var issue of repository.issues) {
+      if (issue.id === notificationId) {
+        window.open(issue.url);
+        return;
+      }
+    }
+  }
+});
+
+chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
+  if (buttonIndex === 1) {
+    window.open("https://github.com/notifications");
+    return;
+  }
+  
+  for (var repository of model.repositories) {
+    for (var issue of repository.issues) {
+      if (issue.id === notificationId) {
+        window.open(issue.url);
+        return;
+      }
+    }
+  }
+});
+
 document.addEventListener("modelchange", function (event) {
-  var count = event.detail.unreadNotificationCount;
-  setBrowserActionBadge("#33ff99", count === "0" ? null : count, "GitHub Notifications: " + count);
-  var title = "GitHub Notifications: ";
-  chrome.notifications.create({ type: "basic", iconUrl: "/icon-128.png", title: title + count, message: title, contextMessage: count, eventTime: new Date().getTime() });
+  model = event.detail;
+  setBrowserActionBadge("#33ff99", model.unreadNotificationCount === "0" ? null : model.unreadNotificationCount, "GitHub Notifications: " + model.unreadNotificationCount);
+});
+
+document.addEventListener("issueadded", function (event) {
+  chrome.notifications.create(event.detail.id,
+  {
+    type: "basic",
+    iconUrl: "/icon-128.png",
+    title: event.detail.repositoryName,
+    message: event.detail.name,
+    eventTime: new Date().getTime(),
+    buttons: [{title: "View notification"}, {title: "View all notifications"}]
+  });
+});
+
+document.addEventListener("issueremoved", function (event) {
+  
 });
--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,28 @@
+var oldIssues = {};
+
+function dispatchDiffs(model) {
+  var newIssues = {};
+  
+  for (var repository of model.repositories) {
+    for (var issue of repository.issues) {
+      newIssues[repository.name + "#" + issue.id] = issue;
+    }
+  }
+  
+  var issueIds = Object.keys(oldIssues).concat(Object.keys(newIssues));
+  for (var issueId of issueIds) {
+    if (!oldIssues[issueId]) {
+      if (newIssues[issueId]) {
+        document.dispatchEvent(new CustomEvent("issueadded", { detail: newIssues[issueId] }));
+      }
+    } else if (!newIssues[issueId]) {
+      document.dispatchEvent(new CustomEvent("issueremoved", { detail: oldIssues[issueId] }));
+    }
+  }
+  
+  oldIssues = newIssues;
+}
+
 function scrapeModel() {
   return new Promise(function (resolve, reject) {
     window.fetch("https://github.com/notifications", { credentials: "include" })
@@ -11,15 +36,27 @@ function scrapeModel() {
               userName: document.querySelector(".css-truncate-target").textContent,
               unreadNotificationCount: document.querySelector(".count").textContent,
               repositories: Array.prototype.map.call(document.querySelectorAll(".notifications-list .boxed-group"), function (node) {
+                var repositoryName = node.querySelector("h3 a").textContent;
                 return {
-                  name: node.querySelector("h3 a").textContent,
+                  name: repositoryName,
                   issues: Array.prototype.map.call(node.querySelectorAll("li.js-notification"), function (node) {
-                    var unsubscribeFormNode = node.querySelector(".js-delete-notification");
-                    return {
-                      name: node.querySelector("a").textContent.trim(),
-                      unsubscribePostUrl: unsubscribeFormNode.getAttribute("action"),
-                      unsubscribePostNonce: unsubscribeFormNode.getAttribute("data-form-nonce")
-                    };
+                    var aNode = node.querySelector(".js-notification-target");
+                    var issueName = aNode.textContent.trim();
+                    var url = aNode.getAttribute("href");
+                    var id = url.substr(url.lastIndexOf("/") + 1);
+                    var deleteFormNode = node.querySelector(".js-delete-notification");
+                    var deletePostUrl = deleteFormNode.getAttribute("action");
+                    var deletePostNonce = deleteFormNode.getAttribute("data-form-nonce");
+                    var deletePostToken = deleteFormNode.querySelector("input[name=authenticity_token]").getAttribute("value");
+                    var muteFormNode = node.querySelector(".js-mute-notification");
+                    var mutePostUrl = muteFormNode.getAttribute("action");
+                    var mutePostNonce = muteFormNode.getAttribute("data-form-nonce");
+                    var mutePostToken = muteFormNode.querySelector("input[name=authenticity_token]").getAttribute("value");
+                    var unmuteFormNode = node.querySelector(".js-unmute-notification");
+                    var unmutePostUrl = unmuteFormNode.getAttribute("action");
+                    var unmutePostNonce = unmuteFormNode.getAttribute("data-form-nonce");
+                    var unmutePostToken = unmuteFormNode.querySelector("input[name=authenticity_token]").getAttribute("value");
+                    return { repositoryName, name: issueName, url, id, deletePostUrl, deletePostNonce, deletePostToken, mutePostUrl, mutePostNonce, mutePostToken, unmutePostUrl, unmutePostNonce, unmutePostToken };
                   })
                 };
               })
@@ -39,6 +76,7 @@ function dispatchModel() {
   scrapeModel()
     .then(function (model) {
       document.dispatchEvent(new CustomEvent("modelchange", { detail: model }));
+      dispatchDiffs(model);
     })
     .catch(function (error) {
       console.error("Everything went awry.", error);
@@ -61,18 +99,22 @@ function setBrowserActionBadge(backgroundColor, text, title) {
         webSocket.send("subscribe:notification-changed:" + model.userName);
         dispatchModel();
       };
-    
+
       webSocket.onmessage = function (message) {
         console.debug("WebSocket message.", message.data);
-        dispatchModel();
+        var data = JSON.parse(message.data);
+        switch (data[0]) {
+          case ("notification-changed:" + model.userName): { dispatchModel(); break; }
+          default: console.error("Unknown WebSocket message", data);
+        }
       };
-    
+
       webSocket.onerror = function (error) {
         console.error("WebSocket error.", error);
         setBrowserActionBadge("#99ff33", "!", error.toString());
         // TODO: Refresh by calling `refreshWebSocket` as per #6.
       };
-    
+
       webSocket.onclose = function () {
         console.debug("WebSocket close.");
       };
